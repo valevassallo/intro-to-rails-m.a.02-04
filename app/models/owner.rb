821371a5d289c0dd656a9b5c5fecf824db918323
Valeria Vassallo <46938066+valevassallo@users.noreply.github.com>
2019-04-05 05:46:40 -0500

    Update owner.rb

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,5 @@
 class Owner < ApplicationRecord
+    has_many :patients
     validates_each :birthday do |record, attr, value|
         record.errors.add attr, "proves you're not yet an adult" if value > (18.years.ago).to_date
     end
